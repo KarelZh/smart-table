bd93b852d738ceb81bd10aea760456255754d333
Dzhoni <karel.zhenya1257@mail.ru>
2026-06-24 14:03:33 -0700

    Исправление поиска

--- src/components/searching.js
+++ src/components/searching.js
@@ -1,22 +1,22 @@
-import {rules, createComparison} from "../lib/compare.js";
+import { createComparison, rules } from "../lib/compare.js";
 
+export function initSearching(elements, searchField) {
+  // @todo: #5.1 — настроить компаратор
+  const searchCompare = createComparison(
+    ['skipEmptyTargetValues'], // Игнорируем пустые значения
+    [rules.searchMultipleFields(searchField, ['date', 'customer', 'seller'], false)]
+  );
 
-export function initSearching(searchField) {
-    // @todo: #5.1 — настроить компаратор
-    const searchCompare = createComparison(
-        ['skipEmptyTargetValues'], // Игнорируем пустые значения
-        [rules.searchMultipleFields(searchField, ['date', 'customer', 'seller'], false)]
-    );
-    return (data, state, action) => {
-        // @todo: #5.2 — применить компаратор
-        const searchValue = state[searchField] || ''.trim(); // Получаем значение поиска из state
+  return (data, state, action) => {
+    // @todo: #5.2 — применить компаратор
+    const searchValue = state[searchField] || ''.trim(); // Получаем значение поиска из state
 
-        // Если строка поиска пустая — возвращаем данные без изменений
-        if (!searchValue) {
-          return data;
-        }
-    
-        // Применяем компаратор: фильтруем данные по значению поиска
-        return data.filter(row => searchCompare(row, { [searchField]: searchValue }));
+    // Если строка поиска пустая — возвращаем данные без изменений
+    if (!searchValue) {
+      return data;
     }
+
+    // Применяем компаратор: фильтруем данные по значению поиска
+    return data.filter(row => searchCompare(row, { [searchField]: searchValue }));
+  };
 }
